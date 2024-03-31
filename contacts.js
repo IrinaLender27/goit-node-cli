9f8f2db7207c4ad04214372f4424af05107c7985
IrinaLender27 <irina.lender27@gmail.com>
2024-03-31 23:49:07 +0300

    update

--- contacts.js
+++ contacts.js
@@ -2,14 +2,56 @@ import { promises as fs } from "fs";
 import path from "path";
 
 const contactsPath = path.join("db", "contacts.json");
+
 async function listContacts() {
   try {
     const readResult = await fs.readFile(contactsPath);
     const contactsObject = JSON.parse(readResult);
-    console.log({ readResult });
     return contactsObject;
   } catch (error) {
-    error;
+    return error;
   }
 }
-async function getContactById(contactId) {}
+
+async function getContactById(contactId) {
+  try {
+    const contactsObject = await listContacts();
+    const contactById = contactsObject.find(
+      (contact) => contact.id === contactId
+    );
+    return contactById || null;
+  } catch (error) {
+    return error;
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const contacts = await listContacts();
+    const removedContactIndex = contacts.findIndex(
+      (contact) => contact.id === contactId
+    );
+    if (removedContactIndex !== -1) {
+      const removedContact = contacts.splice(removedContactIndex, 1)[0];
+      await fs.writeFile(contactsPath, JSON.stringify(contacts));
+      return removedContact;
+    } else {
+      return null;
+    }
+  } catch (error) {
+    return error;
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const contactObject = await listContacts();
+    const newContact = { id: Date.now(), name, email, phone };
+    contactObject.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contactObject));
+    return newContact;
+  } catch (error) {
+    return error;
+  }
+}
+export { listContacts, getContactById, removeContact, addContact };
--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 import { program } from "commander";
+import {
+  getContactById,
+  addContact,
+  listContacts,
+  removeContact,
+} from "./contacts.js";
+
 program
   .option("-a, --action <type>", "choose action")
   .option("-i, --id <type>", "user id")
@@ -10,23 +17,26 @@ program.parse();
 
 const options = program.opts();
 
-// TODO: рефакторити
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case "list":
-      // ...
+      const contactsObject = await listContacts();
+      console.table(contactsObject);
       break;
 
     case "get":
-      // ... id
+      const contactById = await getContactById(id);
+      console.log(contactById);
       break;
 
     case "add":
-      // ... name email phone
+      const add = await addContact(name, email, phone);
+      console.log(add);
       break;
 
     case "remove":
-      // ... id
+      const removed = await removeContact(id);
+      console.log(removed);
       break;
 
     default:
